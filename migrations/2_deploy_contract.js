4437395a8a576c76c7b76aaa69a609fb3467dd14
ahmedali8 <imahmedalibhatti@gmail.com>
2021-05-08 05:26:32 +0500

    updated and deployed smart contracts to rinkeby and ropsten

--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -1,6 +1,6 @@
 var Petshop = artifacts.require("Petshop");
 
-module.exports = async function (deployer, _network, [owner, ...accounts]) {
+module.exports = async function (deployer, _network, [owner]) {
   // https://ipfs.io/ipfs/${hash}
   await deployer.deploy(Petshop, "Petshop", "PET", "https://ipfs.io/ipfs/", {
     from: owner,
